d261f31a4b6783c64766c19beaa7a141dde1d86c
IronGG <adclaren1@gmail.com>
2020-11-25 14:04:14 +0100

    dbCreation
    dbCreation

---
+++ ressource/database/bd_p_prod.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mer 25 Novembre 2020 à 13:03
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bd_p_prod`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_reservation`
+--
+
+CREATE TABLE `t_reservation` (
+  `idReservation` int(11) NOT NULL,
+  `resDate` date NOT NULL,
+  `resHeure` tinyint(1) NOT NULL,
+  `resPlat` tinyint(1) NOT NULL,
+  `resTable` tinyint(1) NOT NULL,
+  `fkUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `idUser` int(11) NOT NULL,
+  `useUsername` varchar(64) NOT NULL,
+  `useEmail` varchar(128) NOT NULL,
+  `useFirstName` varchar(64) NOT NULL,
+  `useLastName` varchar(64) NOT NULL,
+  `usePassword` varchar(200) NOT NULL,
+  `useRole` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `t_reservation`
+--
+ALTER TABLE `t_reservation`
+  ADD PRIMARY KEY (`idReservation`),
+  ADD KEY `fkUser` (`fkUser`);
+
+--
+-- Index pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `t_reservation`
+--
+ALTER TABLE `t_reservation`
+  MODIFY `idReservation` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `t_reservation`
+--
+ALTER TABLE `t_reservation`
+  ADD CONSTRAINT `t_reservation_ibfk_1` FOREIGN KEY (`fkUser`) REFERENCES `t_user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
