a992a1ce01d3f5ca1d73141095ccd0313fc3cbbc
IronGG <adclaren1@gmail.com>
2020-12-16 13:00:08 +0100

    Weekly design update

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -250,6 +250,21 @@ class HomeController extends Controller {
      *
      * @return string
      */
+    private function ContactAction() {
+
+        $view = file_get_contents('view/page/Contact.php');
+        ob_start();
+        eval('?>' . $view);
+        $content = ob_get_clean();
+
+        return $content;
+    }
+
+            /**
+     * Display Contact Action
+     *
+     * @return string
+     */
     private function OptionAction() {
 
         $view = file_get_contents('view/page/Option.php');
