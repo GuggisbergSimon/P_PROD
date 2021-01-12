69a42fa42581c205f2f89a0c5a07a0d1d89d626b
Ataor <slyguggi@gmail.com>
2021-01-12 12:41:44 +0100

    cleaned src and src dependencies

--- view/head.php
+++ view/head.php
@@ -16,8 +16,8 @@ if($_SESSION == array()){
 	<meta charset="UTF-8">
 	<title>ETML - Restaurant</title>
 	<link href="resources/css/style.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<?php 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <?php
 	if(array_key_exists('role', $_SESSION) && $_SESSION['role'] >= 50){
 		echo '<style>';
 		echo '.topnav a:hover { background-color: indigo; }';
